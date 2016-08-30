4ccc6ef3edc48f8a039067ece785cf37ceb34fb4
Jack Jansen <Jack.Jansen@cwi.nl>
2016-08-30 14:35:23 +0200

    Getting started on state progression

--- timelineService/document.py
+++ timelineService/document.py
@@ -2,6 +2,8 @@ import sys
 import urllib
 import xml.etree.ElementTree as ET
 
+DEBUG=True
+
 class NameSpace:
     def __init__(self, namespace, url):
         self.namespace = namespace
@@ -15,6 +17,11 @@ class NameSpace:
         
     def __contains__(self, str):
         return str.startswith('{'+self.url+'}')
+        
+    def localTag(self, str):
+        if str in self:
+            return str[len(self.url)+2:]
+        return str
 
 NS_TIMELINE = NameSpace("tl", "http://jackjansen.nl/timelines")
 NS_2IMMERSE = NameSpace("tim", "http://jackjansen.nl/2immerse")
@@ -22,10 +29,24 @@ NAMESPACES = {}
 NAMESPACES.update(NS_TIMELINE.ns())
 NAMESPACES.update(NS_2IMMERSE.ns())
 
+class State:
+    idle = "idle"
+    initing = "initing"
+    inited = "inited"
+    starting = "starting"
+    started = "started"
+    stopping = "stopping"
+    stopped = "stopped"
+    terminating = "terminating"
+    
 class DummyDelegate:
-    def __init__(self, elt):
+    def __init__(self, elt, document):
         self.elt = elt
-        self.state = "idle"
+        self.document = document
+        self.state = State.idle
+        
+    def __repr__(self):
+        return 'Delegate(%s)' % self.document.getXPath(self.elt)
         
     def checkAttributes(self):
         pass
@@ -34,23 +55,37 @@ class DummyDelegate:
         pass
         
     def setState(self, state):
+        if DEBUG: print self, ': new state', state
         self.state = state
+        parentElement = self.document.getParent(self.elt)
+        #print 'xxxjack parent is', parentElement
+        if parentElement is not None:
+            parentElement.delegate.reportChildState(self.elt, self.state)
+            
+    def reportChildState(self, child, childState):
+        pass
         
     def init(self):
-        assert self.state == "idle"
-        self.setState("inited")
+        assert self.state == State.idle
+        self.setState(State.initing)
+        self.setState(State.inited)
         
     def start(self):
-        assert self.state == "inited"
-        self.setState("started")
+        assert self.state == State.inited
+        self.setState(State.starting)
+        #self.setState(State.started)
+        #self.setState(State.stopping)
+        self.setState(State.stopped)
         
     def stop(self):
-        assert self.state in {"inited", "started"}
-        self.setState("stopped")
+        assert self.state in {State.inited, State.started}
+        self.setState(State.stopping)
+        self.setState(State.stopped)
         
     def terminate(self):
-        assert self.state == "stopped"
-        self.setState("idle")
+        assert self.state == State.stopped
+        self.setState(State.terminating)
+        self.setState(State.idle)
         
 class ErrorDelegate(DummyDelegate):
     def __init__(self, elt):
@@ -75,9 +110,35 @@ class TimelineDelegate(DummyDelegate):
             for child in self.elt:
                 if child.tag in NS_2IMMERSE and not child.tag in self.ALLOWED_CHILDREN:
                     print >>sys.stderr, "* Error: element", self.elt.tag, "cannot have child of type", child.tag
-            
-class DocumentDelegate(TimelineDelegate):
+         
+class SingleChildDelegate(TimelineDelegate):
     EXACT_CHILD_COUNT=1
+
+    def init(self):
+        assert self.state == State.idle
+        self.setState(State.initing)
+        self.elt[0].delegate.init()
+        self.setState(State.inited)
+        
+    def start(self):
+        assert self.state == State.inited
+        self.setState(State.starting)
+        self.elt[0].delegate.start()
+        self.setState(State.started)
+        
+    def stop(self):
+        assert self.state in {State.inited, State.started}
+        self.setState(State.stopping)
+        self.elt[0].delegate.stop()
+        self.setState(State.stopped)
+        
+    def terminate(self):
+        assert self.state == State.stopped
+        self.setState(State.terminating)
+        self.elt[0].delegate.terminate()
+        self.setState(State.idle)
+
+class DocumentDelegate(SingleChildDelegate):
     ALLOWED_CHILDREN={
         NS_TIMELINE("par"),
         NS_TIMELINE("seq"),
@@ -94,6 +155,30 @@ class ParDelegate(TimeElementDelegate):
         NS_TIMELINE("sync"),
         }
         
+    def init(self):
+        assert self.state == State.idle
+        self.setState(State.initing)
+        for child in self.elt: child.delegate.init()
+        self.setState(State.inited)
+        
+    def start(self):
+        assert self.state == State.inited
+        self.setState(State.starting)
+        for child in self.elt: child.delegate.start()
+        self.setState(State.started)
+        
+    def stop(self):
+        assert self.state in {State.inited, State.started}
+        self.setState(State.stopping)
+        for child in self.elt: child.delegate.stop()
+        self.setState(State.stopped)
+        
+    def terminate(self):
+        assert self.state == State.stopped
+        self.setState(State.terminating)
+        for child in self.elt: child.delegate.terminate()
+        self.setState(State.idle)
+
 class SeqDelegate(TimeElementDelegate):
     pass
     
@@ -101,11 +186,17 @@ class RefDelegate(TimeElementDelegate):
     EXACT_CHILD_COUNT=0
     pass
     
-class ConditionalDelegate(TimelineDelegate):
-    EXACT_CHILD_COUNT=1
+class ConditionalDelegate(SingleChildDelegate):
     ALLOWED_ATTRIBUTES = {
         NS_TIMELINE("expr")
         }
+
+    def start(self):
+        assert self.state == State.inited
+        self.setState(State.starting)
+        print 'xxxjack assuming expr is True for', self
+        self.elt[0].delegate.start()
+        self.setState(State.started)
         
 class SleepDelegate(TimeElementDelegate):
     ALLOWED_ATTRIBUTES = TimeElementDelegate.ALLOWED_ATTRIBUTES | {
@@ -132,11 +223,35 @@ class Document:
     def __init__(self):
         self.tree = None
         self.root = None
+        self.parentMap = {}
         
     def load(self, url):
         fp = urllib.urlopen(url)
         self.tree = ET.parse(fp)
         self.root = self.tree.getroot()
+        self.parentMap = {c:p for p in self.tree.iter() for c in p}
+        
+    def getParent(self, elt):
+        return self.parentMap.get(elt)
+        
+    def getXPath(self, elt):
+        parent = self.getParent(elt)
+        if parent is None:
+            return '/'
+        index = 0
+        for ch in parent:
+            if ch is elt:
+                break
+            if ch.tag == elt.tag:
+                index += 1
+        rv = self.getXPath(parent)
+        if rv != '/':
+            rv += '/'
+        tagname = NS_TIMELINE.localTag(elt.tag)
+        rv += tagname
+        if index:
+            rv = rv + '[%d]' % (index+1)
+        return rv
         
     def dump(self, fp):
         self.tree.write(fp)
@@ -145,7 +260,7 @@ class Document:
         for elt in self.tree.iter():
             if not hasattr(elt, 'delegate'):
                 klass = self._getDelegate(elt.tag)
-                elt.delegate = klass(elt)
+                elt.delegate = klass(elt, self)
                 elt.delegate.checkAttributes()
                 elt.delegate.checkChildren()
                 
@@ -154,6 +269,9 @@ class Document:
                 return DummyDelegate
         return DELEGATE_CLASSES.get(tag, ErrorDelegate)
             
+    def run(self):
+        self.root.delegate.init()
+        self.root.delegate.start()
             
         
 def main():
@@ -161,6 +279,8 @@ def main():
     d.load(sys.argv[1])
     d.addDelegates()
     d.dump(sys.stdout)
+    print '--------------------'
+    d.run()
     
 if __name__ == '__main__':
     main()
