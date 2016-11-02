26c19acab64bf7cd539cbbccc080f884d17fc463
Jack Jansen <Jack.Jansen@cwi.nl>
2016-11-02 12:25:59 +0100

    Refactored a bit

---
+++ timelineService/attributeChecker.py
@@ -0,0 +1,38 @@
+import sys
+
+#
+# Specific for 2immerse, needs to be updated as DMApp Component Classes are added and extended.
+#
+REQUIRED_TIC_ATTRIBUTES={
+    "video" : ["mediaUrl"],
+    "scroll-text" : [],
+    "image" : ["mediaUrl"],
+    "FallbackClock" : [],
+    }
+    
+ALLOWED_TIC_ATTRIBUTES={
+    "video" : ["mediaUrl", "offset", "startMediaTime", "syncMode", "showControls"],
+    "scroll-text" : ["scriptUrl", "clipMapUrl", "clipId", "offset"],
+    "image" : ["mediaUrl", "objectFit"],
+    "FallbackClock" : ["syncMode", "offset", "startMediaTime"],
+    }
+    
+def checkAttributes(self):
+    if not NS_2IMMERSE("class") in self.elt.keys():
+        print >>sys.stderr, "* Warning: element", self.getXPath(), "misses expected tim:class attribute"
+    if not NS_2IMMERSE("dmappcid") in self.elt.keys():
+        print >>sys.stderr, "* Warning: element", self.getXPath(), "misses expected tim:dmappcid attribute"
+    className = self.elt.get(NS_2IMMERSE("class"))
+    if not className in REQUIRED_TIC_ATTRIBUTES:
+        print >>sys.stderr, "* Warning: element", self.getXPath(), " has unknown tim:class", className
+        return
+    requiredAttributes = REQUIRED_TIC_ATTRIBUTES[className]
+    allowedAttributes = ALLOWED_TIC_ATTRIBUTES[className]
+    for attrName in requiredAttributes:
+        if not NS_2IMMERSE_COMPONENT(attrName) in self.elt.keys():
+            print >>sys.stderr, "* Warning: element", self.getXPath(), "of tim:class", className, "misses expected attribute tic:"+attrName
+    for attrName in self.elt.keys():
+        if attrName in NS_2IMMERSE_COMPONENT:
+            if not NS_2IMMERSE_COMPONENT.localTag(attrName) in allowedAttributes:
+                print >>sys.stderr, "* Warning: element", self.getXPath(), "of tim:class", className, "has unexpected attribute tic:"+NS_2IMMERSE_COMPONENT.localTag(attrName)
+        
--- timelineService/document.py
+++ timelineService/document.py
@@ -44,23 +44,15 @@ NAMESPACES.update(NS_2IMMERSE_COMPONENT.ns())
 for k, v in NAMESPACES.items():
     ET.register_namespace(k, v)
 
+# For attribute checking for 2immerse documents:
+import attributeChecker
+attributeChecker.NS_TIMELINE = NS_TIMELINE
+attributeChecker.NS_TIMELINE_CHECK = NS_TIMELINE_CHECK
+attributeChecker.NS_2IMMERSE = NS_2IMMERSE
+attributeChecker.NS_2IMMERSE_COMPONENT = NS_2IMMERSE_COMPONENT
+
 PRIO_TO_INT = dict(low=0, normal=50, high=100)
 
-#
-# Specific for 2immerse, needs to be updated as DMApp Component Classes are added and extended.
-#
-REQUIRED_TIC_ATTRIBUTES={
-    "video" : ["mediaUrl"],
-    "scroll-text" : [],
-    "image" : ["mediaUrl"],
-    }
-    
-ALLOWED_TIC_ATTRIBUTES={
-    "video" : ["mediaUrl", "offset", "startMediaTime", "syncMode", "showControls"],
-    "scroll-text" : ["scriptUrl", "clipMapUrl", "clipId", "offset"],
-    "image" : ["mediaUrl", "objectFit"],
-    }
-    
 class State:
     idle = "idle"
     initing = "initing"
@@ -534,23 +526,7 @@ class RefDelegate2Immerse(RefDelegate):
     
     def checkAttributes(self):
         RefDelegate.checkAttributes(self)
-        if not NS_2IMMERSE("class") in self.elt.keys():
-            print >>sys.stderr, "* Warning: element", self.getXPath(), "misses expected tim:class attribute"
-        if not NS_2IMMERSE("dmappcid") in self.elt.keys():
-            print >>sys.stderr, "* Warning: element", self.getXPath(), "misses expected tim:dmappcid attribute"
-        className = self.elt.get(NS_2IMMERSE("class"))
-        if not className in REQUIRED_TIC_ATTRIBUTES:
-            print >>sys.stderr, "* Warning: element", self.getXPath(), " has unknown tim:class", className
-            return
-        requiredAttributes = REQUIRED_TIC_ATTRIBUTES[className]
-        allowedAttributes = ALLOWED_TIC_ATTRIBUTES[className]
-        for attrName in requiredAttributes:
-            if not NS_2IMMERSE_COMPONENT(attrName) in self.elt.keys():
-                print >>sys.stderr, "* Warning: element", self.getXPath(), "of tim:class", className, "misses expected attribute tic:"+attrName
-        for attrName in self.elt.keys():
-            if attrName in NS_2IMMERSE_COMPONENT:
-                if not NS_2IMMERSE_COMPONENT.localTag(attrName) in allowedAttributes:
-                    print >>sys.stderr, "* Warning: element", self.getXPath(), "of tim:class", className, "has unexpected attribute tic:"+NS_2IMMERSE_COMPONENT.localTag(attrName)
+        attributeChecker.checkAttributes(self)
         
 class ConditionalDelegate(SingleChildDelegate):
     ALLOWED_ATTRIBUTES = {
