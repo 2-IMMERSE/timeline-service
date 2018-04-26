43e189d2a054551dc6044575cdcc4d32266a819b
Jack Jansen <Jack.Jansen@cwi.nl>
2018-04-26 16:09:07 +0200

    During seek, guess media of unknown duration will last 10 hours. May produce better results than guessing they take 0 seconds.

--- timelineService/document.py
+++ timelineService/document.py
@@ -484,7 +484,7 @@ class FFWDTimeElementDelegate(TimeElementDelegate):
         self.setState(State.started)
         expectedDuration = float(self.elt.get(NS_TIMELINE_CHECK("dur"), 0))
         if expectedDuration == 0:
-            self.logger.warn("%s: syncMode=master element without tlcheck:dur. Guessing at 10 hours." % self.getXPath())
+            self.logger.warning("%s: syncMode=master element without tlcheck:dur. Guessing at 10 hours." % self.getXPath())
             expectedDuration = 36000
         self.clock.schedule(expectedDuration, self._done)
                
