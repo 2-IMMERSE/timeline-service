76ced15a2faaa69238c4852789752f4de94aa5d0
Jack Jansen <Jack.Jansen@cwi.nl>
2018-05-05 00:53:13 +0200

    It is not an error to change a dur attribute on a sleep that is not active. Fixes issue seen during May 4 test.

--- timelineService/document.py
+++ timelineService/document.py
@@ -1000,7 +1000,8 @@ class SleepDelegate(TimeElementDelegate):
         for k in attrsChanged:
             if k == NS_TIMELINE("dur"):
                 dur = self.parseDuration(self.elt.get(NS_TIMELINE("dur")))
-                assert self.startTime != None
+                if self.startTime != None or self.state != started:
+                    continue
                 newSleepEndTime = self.startTime + dur
                 self.document.logger.debug("SleepDelegate(%s): sleepEndTime changed from %.3f to %.3f" % (self.document.getXPath(self.elt), self.sleepEndTime, newSleepEndTime), extra=self.getLogExtra())
                 self.sleepEndTime = newSleepEndTime
