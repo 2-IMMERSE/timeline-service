555cab5ddecaa40f22245e29f42947cd4d212cf5
Jack Jansen <Jack.Jansen@cwi.nl>
2018-09-27 21:03:15 +0200

    Added new documents for testing, basically some interesting tech-samples documents

--- test/test_documents.py
+++ test/test_documents.py
@@ -29,24 +29,63 @@ class TestDocuments(unittest.TestCase):
         outputDocumentData = open(outputDocument).read()
         outputTraceData = open(outputTrace).read()
         
-        if os.path.exists(expectedDocument):
-#            expectedDocumentData = open(expectedDocument).read()
-#            self.assertEqual(outputDocumentData, expectedDocumentData)
-            diffs = xmldiff.main.diff_files(expectedDocument, outputDocument)
-            self.assertEqual(diffs, [])
+        self.assertTrue(os.path.exists(expectedDocument))
+        self.assertTrue(os.path.exists(expectedTrace))
+
+        # Compare XML document output state
+        diffs = xmldiff.main.diff_files(expectedDocument, outputDocument)
+        self.assertEqual(diffs, [])        
+
+        # Compare emitted events
+        outputTraceLines = outputTraceData.splitlines()
+        outputTraceRecords = [eval(x) for x in outputTraceLines]
+        outputTraceRecords.sort()
+        expectedTraceData = open(expectedTrace).read()
+        expectedTraceLines = expectedTraceData.splitlines()
+        expectedTraceRecords = [eval(x) for x in expectedTraceLines]
+        expectedTraceRecords.sort()
+        for o, e in zip(outputTraceRecords, expectedTraceRecords):
+            self.assertEqual(e, o)
         
-        if os.path.exists(expectedTrace):
-            outputTraceLines = outputTraceData.splitlines()
-            outputTraceRecords = [eval(x) for x in outputTraceLines]
-            expectedTraceData = open(expectedTrace).read()
-            expectedTraceLines = expectedTraceData.splitlines()
-            expectedTraceRecords = [eval(x) for x in expectedTraceLines]
-            for o, e in zip(outputTraceRecords, expectedTraceRecords):
-                self.assertEqual(e, o)
         
-        
-    def test_000(self):
+    def test_000_ref(self):
         self._runOne('test_000_ref')
+
+    def test_100_video(self):
+        self._runOne('test_100_video')
+
+    def test_104_images(self):
+        self._runOne('test_104_images')
+
+    def test_107_button_video(self):
+        self._runOne('test_107_button_video')
+
+    def test_108_video_description_images(self):
+        self._runOne('test_108_video_description_images')
+
+    def test_111_update_mp4(self):
+        self._runOne('test_111_update_mp4')
+
+    def test_113_pip_multi(self):
+        self._runOne('test_113_pip_multi')
+
+    def test_124_update_layout(self):
+        self._runOne('test_124_update_layout')
+
+    def test_144_repeat_button(self):
+        self._runOne('test_144_repeat_button')
+
+    def test_148_update_append(self):
+        self._runOne('test_148_update_append')
+
+    def test_202_2video_mp4(self):
+        self._runOne('test_202_2video_mp4')
+
+    def test_203_6video_mp4(self):
+        self._runOne('test_203_6video_mp4')
+
+    def test_300_events(self):
+        self._runOne('test_300_events')
         
 if __name__ == '__main__':
     unittest.main()
