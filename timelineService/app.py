037209ef82f5a1f7b7b213a71fcd13f28520a7eb
Jack Jansen <Jack.Jansen@cwi.nl>
2018-10-02 14:06:53 +0200

    Converted from web.py to flask

--- timelineService/app.py
+++ timelineService/app.py
@@ -7,170 +7,126 @@ import traceback
 import sys
 import json
 import os
-import web
+from gevent.pywsgi import WSGIServer
+from flask import Flask, Response, request, abort, jsonify, make_response
+from werkzeug.exceptions import HTTPException
+
 from . import timeline
 
-        
-urls = (
-    '/timeline/v1/context', 'timelineServerServer',
-    '/timeline/v1/context/(.*)/(.*)', 'timelineServer',
-    '/timeline/v1/context/(.*)', 'timelineServer',
-)
+app = Flask(__name__)
+# app.config.from_object("flask_config")
 
-app = web.application(urls, globals())
+#
+# Disable CORS problems
+#
+def add_cors_headers(response):
+    response.headers['Access-Control-Allow-Origin'] = '*'
+    if request.method == 'OPTIONS':
+        response.headers['Access-Control-Allow-Methods'] = 'DELETE, GET, POST, PUT'
+        headers = request.headers.get('Access-Control-Request-Headers')
+        if headers:
+            response.headers['Access-Control-Allow-Headers'] = headers
+    return response
+app.after_request(add_cors_headers)
 
-def appendCorsHeaders():
-    if sys.version_info[0] < 3:
-        def A(x): return x.encode('ascii')
-    else:
-        def A(x): return x
-    web.ctx.headers.append((A('Allow'), A('GET,PUT,POST,DELETE')))
-    web.ctx.headers.append((A('Access-Control-Allow-Methods'), A('GET,PUT,POST,DELETE')))
-    web.ctx.headers.append((A('Access-Control-Allow-Origin'), A('*')))
+     
+API_ROOT = "/timeline/v1/context"
 
-class timelineServerServer(object):
-    """Toplevel REST interface: create new timeline servers and get the identity of existing ones"""
-    # Note this is hack-y: post/get work with both /timelineServer and /timelineServer/createTimeline
-    # and do the same. To be fixed.
-
-    def GET(self):
-        rv = timeline.Timeline.getAll()
-        # HACK WARNING: GET with arguments is treated as POST
-        if web.input():
-            return self.POST()
-        appendCorsHeaders()
-        web.header("Content-Type", "application/json")
-        return json.dumps(rv)
-
-    def POST(self):
-        appendCorsHeaders()
-        args = web.input()
-
-        # XXX Ugly hack because I don't know what else to do
-        timelineServiceUrl = os.getenv("TIMELINE_SERVICE_URL")
-
-        if timelineServiceUrl is None:
-            timelineServiceUrl = web.ctx.env.get("wsgi.url_scheme", "http") + "://" + web.ctx.env.get("HTTP_HOST")
-
+@app.route(API_ROOT, methods=["GET", "POST"])
+def allContexts():
+    print('xxxjack allContext args', request.args, 'form', request.form, 'data', request.data)
+    timelineServiceUrl = "http://example.com"
+    if request.method == "POST" or request.args:
+        if request.form:
+            args = request.form.to_dict(flat=True)
+        else:
+            args = request.args.to_dict(flat=True)
+        print('xxxjack args', args)
         rv = timeline.Timeline.createTimeline(timelineServiceUrl=timelineServiceUrl, **args)
+        return Response(json.dumps(rv), mimetype="application/json")
+    else:
+        rv = timeline.Timeline.getAll()
+        return Response(json.dumps(rv), mimetype="application/json")
+    
+@app.route(API_ROOT + "/<string:contextId>/<string:verb>", methods=["GET"])
+def getContextVerb(contextId, verb):
+    args = request.args.to_dict(flat=True)
+    tl = timeline.Timeline.get(contextId)
+    if not tl:
+        abort(make_response("No such context: %s" % contextId, 404))
+    method = getattr(tl, verb, None)
+    if not method:
+        abort(make_response("No such method: %s" % contextId, 404))
+    try:
+        rv = method(**args)
+    except HTTPException:
+        raise
+    except:
+        status = "500 Internal server error: %s" % ' '.join(traceback.format_exception_only(sys.exc_info()[0], sys.exc_info()[1]))
+        traceback.print_exc()
+        abort(make_response(status, 500))
+    return Response(json.dumps(rv), mimetype="application/json")
 
-        if rv == None or rv == '':
-            web.ctx.status = '204 No Content'
-            return ''
-        web.header("Content-Type", "application/json")
-        return json.dumps(rv)
 
-class timelineServer(object):
-    """Per-timeline service. Need to work out which verbs allow get/put/post and how to encode that."""
+@app.route(API_ROOT + "/<string:contextId>/<string:verb>", methods=["PUT"])
+def putContextVerb(contextId, verb):
+    # Argument passing is convoluted...
+    args = {}
+    if request.args:
+        args.update(request.args.to_dict(flat=True))
+    if request.form:
+        args.update(request.form.to_dict(flat=True))
+    if request.is_json:
+        jargs = request.get_json()
+        if type(jargs) == type("") or type(jargs) == type(u""):
+            # xxxjack Bug workaround, 21-Dec-2016
+            jargs = json.loads(jargs)
+        # Usually the json is an object/dict, sometimes not.
+        if type(jargs) == type({}):
+            args.update(jargs)
+        else:
+            args['postData'] = jargs
+    tl = timeline.Timeline.get(contextId)
+    if not tl:
+        abort(make_response("No such context: %s" % contextId, 404))
+    method = getattr(tl, verb, None)
+    if not method:
+        abort(make_response("No such method: %s" % contextId, 404))
+    try:
+        rv = method(**args)
+    except HTTPException:
+        raise
+    except:
+        status = "500 Internal server error: %s" % ' '.join(traceback.format_exception_only(sys.exc_info()[0], sys.exc_info()[1]))
+        traceback.print_exc()
+        abort(make_response(status, 500))
+    return Response(json.dumps(rv), mimetype="application/json")
 
-    def OPTIONS(self, *args):
-        appendCorsHeaders()
-        return ''
+@app.route(API_ROOT + "/<string:contextId>/<string:verb>", methods=["POST"])
+def postContextVerb(contextId, verb):
+    return putContextVerb(contextId, verb)
 
-    def GET(self, contextId, verb=None):
-        appendCorsHeaders()
-        if not verb:
-            return web.badrequest()
-        args = web.input()
-        tl = timeline.Timeline.get(contextId)
-        if not tl:
-            return web.notfound("404 No such context: %s" % contextId)
-        method = getattr(tl, verb, None)
-        if not method:
-            return web.notfound("404 No such verb: %s" % verb)
-        try:
-            rv = method(**args)
-        except web.HTTPError:
-            raise
-        except:
-            web.ctx.status = "500 Internal server error: %s" % ' '.join(traceback.format_exception_only(sys.exc_info()[0], sys.exc_info()[1]))
-            traceback.print_exc()
-            return ''
-        web.header("Content-Type", "application/json")
-        return json.dumps(rv)
-
-    def PUT(self, contextId, verb=None):
-        appendCorsHeaders()
-        if not verb:
-            return web.badrequest()
-        args = dict(web.input())
-        # PUT gets some data as a JSON body, sometimes...
-        data = web.data()
-        if data:
-            args2 = json.loads(data)
-            if type(args2) == type('') or type(args2) == type(u''):
-                # xxxjack Bug workaround, 21-Dec-2016
-                args2 = json.loads(args2)
-            args.update(args2)
-        tl = timeline.Timeline.get(contextId)
-        if not tl:
-            return web.notfound("404 No such context: %s" % contextId)
-        method = getattr(tl, verb, None)
-        if not method:
-            return web.notfound("404 No such verb: %s" % verb)
-        try:
-            rv = method(**args)
-        except web.HTTPError:
-            raise
-        except:
-            web.ctx.status = "500 Internal server error: %s" % ' '.join(traceback.format_exception_only(sys.exc_info()[0], sys.exc_info()[1]))
-            traceback.print_exc()
-            return ''
-        if rv == None or rv == '':
-            web.ctx.status = '204 No Content'
-            return ''
-        web.header("Content-Type", "application/json")
-        return json.dumps(rv)
-
-    def POST(self, contextId, verb=None):
-        appendCorsHeaders()
-        if not verb:
-            return web.badrequest()
-        args = web.input()
-        data = web.data()
-        if data:
-            args2 = json.loads(data)
-            if type(args2) == type('') or type(args2) == type(u''):
-                # xxxjack Bug workaround, 21-Dec-2016
-                args2 = json.loads(args2)
-            # Sometimes it is an object, sometimes it is something else (like an array)
-            if type(args2) != type({}):
-                args2 = {'postData' : args2}
-            args.update(args2)
-        tl = timeline.Timeline.get(contextId)
-        if not tl:
-            return web.notfound("404 No such context: %s" % contextId)
-        method = getattr(tl, verb, None)
-        if not method:
-            return web.notfound("404 No such verb: %s" % verb)
-        try:
-            rv = method(**args)
-        except web.HTTPError:
-            raise
-        except:
-            web.ctx.status = "500 Internal server error: %s" % ' '.join(traceback.format_exception_only(sys.exc_info()[0], sys.exc_info()[1]))
-            traceback.print_exc()
-            return ''
-        if rv == None or rv == '':
-            web.ctx.status = '204 No Content'
-            return ''
-        web.header("Content-Type", "application/json")
-        return json.dumps(rv)
-
-    def DELETE(self, contextId, verb=None):
-        appendCorsHeaders()
-        args = web.input()
-        if verb != None or args:
-            return web.badrequest()
-        tl = timeline.Timeline.get(contextId)
-        if not tl:
-            return web.notfound("404 No such context: %s" % contextId)
+@app.route(API_ROOT + "/<string:contextId>", methods=["DELETE"])
+def deleteContext(contextId):
+    tl = timeline.Timeline.get(contextId)
+    if tl:
         tl.delete()
-        web.ctx.status = '204 No Content'
-        return ''
+    return ''
+
+class Server:
+    def __init__(self, port):
+        self.port = port if port else 8080
         
+        self.server = WSGIServer(("0.0.0.0", self.port), app)
+        
+    def run(self):
+        print("timelineServicer: running on port %d" % self.port)
+        self.server.serve_forever()
+        
+_singleton = None
 def app_singleton(port=None):
-    del sys.argv[1:]
-    if port:
-        sys.argv.append(str(args.port))
-    return app
+    global _singleton
+    if _singleton == None:
+        _singleton = Server(port)
+    return _singleton
+    
